38866f8bbcf294200f2cd73242d09ba0e561c371
Marco A L Barbosa <malbarbo@gmail.com>
2024-11-09 11:09:33 -0300

    Make sgleam/check special

--- compiler-core/src/javascript/expression.rs
+++ compiler-core/src/javascript/expression.rs
@@ -837,7 +837,7 @@ impl<'module> Generator<'module> {
     fn call_with_doc_args<'a>(
         &mut self,
         fun: &'a TypedExpr,
-        arguments: Vec<Document<'a>>,
+        mut arguments: Vec<Document<'a>>,
     ) -> Output<'a> {
         match fun {
             // Qualified record construction
@@ -905,6 +905,30 @@ impl<'module> Generator<'module> {
             }
 
             _ => {
+                if let TypedExpr::ModuleSelect {
+                    constructor: ModuleValueConstructor::Fn { module, .. },
+                    location,
+                    ..
+                } = fun
+                {
+                    if module == "sgleam/check" {
+                        let module = self.module_name.clone().to_doc().surround('"', '"');
+                        let function = self
+                            .function_name
+                            .clone()
+                            .unwrap_or_default()
+                            .to_doc()
+                            .surround('"', '"');
+                        let line_number = self.line_numbers.line_number(location.start).to_doc();
+                        arguments = arguments
+                            .into_iter()
+                            .map(|arg| arg.surround("() => { return ", " }"))
+                            .collect();
+                        arguments.push(module);
+                        arguments.push(function);
+                        arguments.push(line_number);
+                    }
+                };
                 let fun = self.not_in_tail_position(|r#gen| {
                     let is_fn_literal = matches!(fun, TypedExpr::Fn { .. });
                     let fun = r#gen.wrap_expression(fun)?;
