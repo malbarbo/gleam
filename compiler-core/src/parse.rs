650855f48d10f9795c936bd8e1a812fe4c64881b
Marco A L Barbosa <malbarbo@gmail.com>
2024-11-29 19:19:29 -0300

    Make some Parser methods pub

--- compiler-core/src/parse.rs
+++ compiler-core/src/parse.rs
@@ -115,7 +115,7 @@ enum InternalAttribute {
 }
 
 #[derive(Debug, Default)]
-struct Attributes {
+pub struct Attributes {
     target: Option<Target>,
     deprecated: Deprecation,
     external_erlang: Option<(EcoString, EcoString, SrcSpan)>,
@@ -256,7 +256,7 @@ where
     // place and instead we collect LexErrors in `self.lex_errors` and attempt to continue parsing.
     // Once parsing has returned we want to surface an error in the order:
     // 1) LexError, 2) ParseError, 3) More Tokens Left
-    fn ensure_no_errors_or_remaining_input<A>(
+    pub fn ensure_no_errors_or_remaining_input<A>(
         &mut self,
         parse_result: Result<A, ParseError>,
     ) -> Result<A, ParseError> {
@@ -298,7 +298,7 @@ where
         }
     }
 
-    fn parse_definition(&mut self) -> Result<Option<TargetedDefinition>, ParseError> {
+    pub fn parse_definition(&mut self) -> Result<Option<TargetedDefinition>, ParseError> {
         let mut attributes = Attributes::default();
         let location = self.parse_attributes(&mut attributes)?;
 
@@ -1023,7 +1023,7 @@ where
         }
     }
 
-    fn parse_statement(&mut self) -> Result<Option<UntypedStatement>, ParseError> {
+    pub fn parse_statement(&mut self) -> Result<Option<UntypedStatement>, ParseError> {
         match self.tok0.take() {
             Some((start, Token::Use, end)) => {
                 self.advance();
@@ -3321,7 +3321,7 @@ functions are declared separately from types.";
     }
 
     // Parse a series by repeating a parser, and possibly a separator
-    fn series_of<A>(
+    pub fn series_of<A>(
         &mut self,
         parser: &impl Fn(&mut Self) -> Result<Option<A>, ParseError>,
         sep: Option<&Token>,
