b6d31402ca9b20abb3b56ce4bcf7386571a12dca
Marco A L Barbosa <malbarbo@gmail.com>
2025-03-17 16:21:26 -0300

    Do not use console.warn

--- compiler-core/templates/prelude.mjs
+++ compiler-core/templates/prelude.mjs
@@ -339,10 +339,6 @@ function bitArrayPrintDeprecationWarning(name, message) {
     return;
   }
 
-  console.warn(
-    `Deprecated BitArray.${name} property used in JavaScript FFI code. ${message}.`,
-  );
-
   isBitArrayDeprecationMessagePrinted[name] = true;
 }
 
